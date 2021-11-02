d9ff8cb07a8a47d89a2ecab7b5bc3dc187d80267
Max Patiiuk <max@patii.uk>
2021-11-02 09:41:34 -0500

    Recreate nging and docker compose configs on state changes

--- pages/api/state/[index]/ping.ts
+++ pages/api/state/[index]/ping.ts
@@ -13,6 +13,12 @@ export default async function handler(
   const user = await getUser(req, res);
   if (typeof user === 'undefined') return;
 
+  const origin = req.headers['origin'];
+  if (typeof origin !== 'string')
+    return res
+      .status(400)
+      .json({ error: '"Origin" request header is missing' });
+
   const state = await getState();
 
   const index = Number.parseInt(req.query.index as string);
@@ -28,8 +34,9 @@ export default async function handler(
       },
       ...state.slice(index + 1),
     ],
-    user
+    user,
+    origin
   );
 
-  return res.status(204);
+  return res.status(204).send('');
 }
--- pages/api/state/index.ts
+++ pages/api/state/index.ts
@@ -9,29 +9,46 @@ import {
   Deployment,
   formalizeState,
 } from '../../../lib/deployment';
+import { createDockerConfig } from '../../../lib/dockerCompose';
+import { createNginxConfig } from '../../../lib/nginx';
 import { RA } from '../../../lib/typescriptCommonTypes';
 import { User } from '../../../lib/user';
 
-const fileName = path.resolve(workingDirectory, 'configuration.json');
+const configurationFile = path.resolve(workingDirectory, 'configuration.json');
+const nginxConfigurationFile = path.resolve(workingDirectory, 'nginx.conf');
+const dockerConfigurationFile = path.resolve(
+  workingDirectory,
+  'docker-compose.override.yml'
+);
 
 export async function getState(): Promise<RA<ActiveDeployment>> {
-  if (!(await fileExists(fileName)))
-    await fs.promises.writeFile(fileName, JSON.stringify([]));
+  if (!(await fileExists(configurationFile)))
+    await fs.promises.writeFile(configurationFile, JSON.stringify([]));
 
   return fs.promises
-    .readFile(fileName)
+    .readFile(configurationFile)
     .then((file) => JSON.parse(file.toString()));
 }
 
 export async function setState(
   deployments: RA<Deployment>,
-  user: User
+  user: User,
+  origin: string
 ): Promise<RA<ActiveDeployment>> {
   const state = await autoDeployPullRequests(
     formalizeState(deployments, await getState()),
     user
   );
-  await fs.promises.writeFile(fileName, JSON.stringify(state));
+
+  await fs.promises.writeFile(configurationFile, JSON.stringify(state));
+  await fs.promises.writeFile(
+    nginxConfigurationFile,
+    createNginxConfig(state, new URL(origin).hostname)
+  );
+  await fs.promises.writeFile(
+    dockerConfigurationFile,
+    createDockerConfig(state)
+  );
   return state;
 }
 
@@ -48,7 +65,15 @@ export default async function handler(
       return res.status(400).json({
         error: 'Invalid request body specified',
       });
-    const state = await setState(request, user);
+
+    const origin = req.headers['origin'];
+    if (typeof origin !== 'string')
+      return res
+        .status(400)
+        .json({ error: '"Origin" request header is missing' });
+
+    const state = await setState(request, user, origin);
+
     res.status(200).json({ data: state });
   } else if (req.method === 'GET')
     getState()
