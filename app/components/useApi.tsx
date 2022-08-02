77c5031cc261a234aae44dd6a699714edfcc9e2a
Max Patiiuk <max@patii.uk>
2022-08-02 12:54:48 -0500

    Fix build time not getting fetched properly

--- app/components/useApi.tsx
+++ app/components/useApi.tsx
@@ -29,25 +29,32 @@ export function useAsync<TYPE>(
   return [value, setValue];
 }
 
-export function useApi<TYPE>(endpoint: string) {
+export function useApi<TYPE>(
+  endpoint: string,
+  format: 'txt' | 'json' = 'json'
+) {
   return useAsync<{ readonly data: TYPE }>(
     React.useCallback(
       async () =>
         fetch(endpoint).then(async (response) => {
           if (response.status === 404)
             throw new Error('API endpoint not found');
+          const textResponse = await response.text();
           try {
-            const state = await response.json();
+            const state =
+              format === 'json'
+                ? JSON.parse(textResponse)
+                : { data: textResponse };
             if (response.status === 200) return state;
             else
               throw new Error(
                 state.error ?? state ?? 'Unexpected Error Occurred'
               );
           } catch {
-            return await response.text();
+            return textResponse;
           }
         }),
-      [endpoint]
+      [endpoint, format]
     )
   );
 }
