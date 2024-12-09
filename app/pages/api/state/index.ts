d2ed582dd30f8cb0193c430299c20b363160b8e6
alec_dev <acwhite211@gmail.com>
2024-12-09 10:59:20 -0600

    remove node-fetch dependency

--- app/pages/api/state/index.ts
+++ app/pages/api/state/index.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'node:fs';
 import path from 'node:path';
-import fetch from 'node-fetch';
 
 import {
   nginxConfDirectory as nginxConfigDirectory,
