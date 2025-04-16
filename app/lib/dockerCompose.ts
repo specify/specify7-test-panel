33c1cfb335ed1c7c80d1d4413dee04e70cde95e9
alec_dev <acwhite211@gmail.com>
2025-04-16 14:27:11 -0500

    running fixes

--- app/lib/dockerCompose.ts
+++ app/lib/dockerCompose.ts
@@ -47,9 +47,12 @@ ${deployments
       - DATABASE_HOST=${process.env.MYSQL_HOST}
       - MASTER_NAME=${process.env.MYSQL_USERNAME}
       - MASTER_PASSWORD=${process.env.MYSQL_PASSWORD}
+      - ASSET_SERVER_URL=${process.env.ASSET_SERVER_URL}
+      - ASSET_SERVER_KEY=${process.env.ASSET_SERVER_KEY}
+      - ASSET_SERVER_COLLECTION=${process.env.ASSET_SERVER_COLLECTION}
       - SECRET_KEY="change this to some unique random string"
-      - REPORT_RUNNER_HOST=report-runner
-      - REPORT_RUNNER_PORT=8080
+      - REPORT_RUNNER_HOST=${process.env.REPORT_RUNNER_HOST}
+      - REPORT_RUNNER_PORT=${process.env.REPORT_RUNNER_PORT}
       - CELERY_BROKER_URL=redis://redis/0
       - CELERY_RESULT_BACKEND=redis://redis/1
       - CELERY_TASK_QUEUE=${deployment.hostname}
@@ -57,8 +60,8 @@ ${deployments
       - LOG_LEVEL=DEBUG
       ${
         deployment.hasInteralSp7ConfigDirectory
-          ? '- THICK_CLIENT_LOCATION=/opt/specify7'
-          : '- THICK_CLIENT_LOCATION=/opt/Specify'
+          ? '- SPECIFY_CONFIG_DIR=/opt/specify7/config'
+          : '- SPECIFY_CONFIG_DIR=/opt/Specify/config'
       }
 
   ${deployment.hostname}-worker:
@@ -68,25 +71,28 @@ ${deployments
     }
     init: true
     restart: unless-stopped
-    ${
-      deployment.hasInteralSp7ConfigDirectory
-        ? ''
-        : `volumes:
-      - "specify${deployment.schemaVersion}:/opt/Specify:ro"`
-    }
     networks:
       - redis
       - database
+    volumes:
+      ${
+        deployment.hasInteralSp7ConfigDirectory
+          ? ''
+          : `- "specify${deployment.schemaVersion}:/opt/Specify:ro"`
+      }
+      - "${deployment.hostname}-static-files:/volumes/static-files"
     environment:
       - LC_ALL=C.UTF-8
       - LANG=C.UTF-8
       - DATABASE_NAME=${deployment.database}
       - DATABASE_HOST=${process.env.MYSQL_HOST}
       - MASTER_NAME=${process.env.MYSQL_USERNAME}
+      - ASSET_SERVER_URL=${process.env.ASSET_SERVER_URL}
+      - ASSET_SERVER_KEY=${process.env.ASSET_SERVER_KEY}
       - MASTER_PASSWORD=${process.env.MYSQL_PASSWORD}
       - SECRET_KEY="change this to some unique random string"
-      - REPORT_RUNNER_HOST=report-runner
-      - REPORT_RUNNER_PORT=8080
+      - REPORT_RUNNER_HOST=${process.env.REPORT_RUNNER_HOST}
+      - REPORT_RUNNER_PORT=${process.env.REPORT_RUNNER_PORT}
       - CELERY_BROKER_URL=redis://redis/0
       - CELERY_RESULT_BACKEND=redis://redis/1
       - CELERY_TASK_QUEUE=${deployment.hostname}
@@ -94,8 +100,8 @@ ${deployments
       - LOG_LEVEL=DEBUG
       ${
         deployment.hasInteralSp7ConfigDirectory
-          ? '- THICK_CLIENT_LOCATION=/opt/specify7'
-          : '- THICK_CLIENT_LOCATION=/opt/Specify'
+          ? '- SPECIFY_CONFIG_DIR=/opt/specify7/config'
+          : '- SPECIFY_CONFIG_DIR=/opt/Specify/config'
       }`
   )
   .join('\n\n')}
