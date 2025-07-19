dd1e6f36cd4e3b547a1516d6a7247c2085f2af8f
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2025-07-18 23:44:06 -0500

    Update [container].ts

--- app/pages/api/logs/[container].ts
+++ app/pages/api/logs/[container].ts
@@ -9,7 +9,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
   try {
     const logs = await getContainerLogs(container);
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Cache-Control', `private, max-age=5000`);
     res.status(200).send(logs);
   } catch (e: any) {
     res.status(500).send(e?.message ?? 'Failed to fetch logs');
