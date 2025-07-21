1b9b2f72be09a319dd71e034f45735b4487eeeaf
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2025-07-20 20:11:58 -0500

    fix: correct calculator icon

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -23,5 +23,14 @@ export const icons = {
   trash: <svg aria-hidden className={iconClassName} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" /></svg>,
   key: <svg aria-hidden className={iconClassName} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 8a6 6 0 01-7.743 5.743L10 14l-1 1-1 1H6v2H2v-4l4.257-4.257A6 6 0 1118 8zm-6-4a1 1 0 100 2 2 2 0 012 2 1 1 0 102 0 4 4 0 00-4-4z" clipRule="evenodd" /></svg>,
   upload: <svg aria-hidden className={iconClassName} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>,
-  calculator: <svg aria-hidden className={iconClassName} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V4a2 2 0 00-2-2H6zm1 2a1 1 0 000 2h6a1 1 0 100-2H7zm6 7a1 1 0 112 0v3a1 1 0 11-2 0v-3zm-3 3a1 1 0 100 2h.01a1 1 0 100-2H10zm-4 0a1 1 0 100 2h.01a1 1 0 100-2H6zm2-3a1 1 0 011-1h.01a1 1 0 110 2H9a1 1 0 01-1-1zm-2 0a1 1 0 100 2h.01a1 1 0 100-2H6zm2-3a1 1 0 011-1h.01a1 1 0 110 2H9a1 1 0 01-1-1zm-2 0a1 1 0 100 2h.01a1 1 0 100-2H6z" clipRule="evenodd" /></svg>,
+  calculator: (
+    <svg aria-hidden className={iconClassName} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <rect x="4" y="3" width="16" height="18" rx="2" strokeWidth="2"/>
+      <rect x="8" y="7" width="8" height="2" rx="1" strokeWidth="2"/>
+      <circle cx="9" cy="13" r="1" fill="currentColor"/>
+      <circle cx="15" cy="13" r="1" fill="currentColor"/>
+      <circle cx="9" cy="17" r="1" fill="currentColor"/>
+      <circle cx="15" cy="17" r="1" fill="currentColor"/>
+    </svg>
+  ),
 } as const;
