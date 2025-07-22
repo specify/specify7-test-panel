7e09ef98a22df3f8753ae6b0f591cb8f9ca68da5
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2025-07-21 21:00:53 -0500

    fix: set redis as host

--- app/lib/queue.ts
+++ app/lib/queue.ts
@@ -1,9 +1,9 @@
 import Bull from 'bull';
 
 export const uploadQueue = new Bull('database-upload', {
-  redis: { host: 'localhost', port: 6379 }
+  redis: { host: 'redis', port: 6379 }
 });
 
 export const cloneQueue = new Bull('database-clone', {
-  redis: { host: 'localhost', port: 6379 }
+  redis: { host: 'redis', port: 6379 }
 });
